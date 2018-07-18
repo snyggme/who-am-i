2ac82049de1a1495d4fb6de44b249239bc2870eb
Glitch (narrow-mine) <none>
2018-07-18 08:24:19 +0000

    🍵🌾 Checkpoint ./server.js:1469898/229

--- server.js
+++ server.js
@@ -18,15 +18,11 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/whoami", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
-// listen for requests :)
+// listen for requests 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
